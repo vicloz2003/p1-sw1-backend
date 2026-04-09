b74cefa24c1b86655252319f0ea5fa36f95b8cc9
vicloz2003 <victoradlozada97@gmail.com>
2026-04-09 17:51:05 -0400

    Initial commit

---
+++ src/main/java/com/ibpm/PoliticaNegocio/PoliticaNegocioApplication.java
@@ -0,0 +1,13 @@
+package com.ibpm.PoliticaNegocio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliticaNegocioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliticaNegocioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibpm/PoliticaNegocio/PoliticaNegocioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibpm.PoliticaNegocio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliticaNegocioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
